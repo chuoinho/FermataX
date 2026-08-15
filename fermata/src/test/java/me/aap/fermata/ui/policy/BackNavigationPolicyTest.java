d62492d76d581fc57b416ca6b369d5599fe38116
chuoinho <108043501+chuoinho@users.noreply.github.com>
2026-08-16 02:01:11 +0700

    Test unified back semantics

--- fermata/src/test/java/me/aap/fermata/ui/policy/BackNavigationPolicyTest.java
+++ fermata/src/test/java/me/aap/fermata/ui/policy/BackNavigationPolicyTest.java
@@ -1,37 +1,14 @@
 package me.aap.fermata.ui.policy;
 
-import static me.aap.fermata.ui.view.BodyLayout.Mode.BOTH;
-import static me.aap.fermata.ui.view.BodyLayout.Mode.FRAME;
 import static me.aap.fermata.ui.policy.BackNavigationPolicy.ActivityBackAction.FINISH;
 import static me.aap.fermata.ui.policy.BackNavigationPolicy.ActivityBackAction.SHOW_NAV_FRAGMENT;
-import static me.aap.fermata.ui.policy.BackNavigationPolicy.PlayerBackAction.LEAVE_VIDEO_MODE;
-import static me.aap.fermata.ui.policy.BackNavigationPolicy.PlayerBackAction.REFRESH_CHROME;
-import static me.aap.fermata.ui.policy.BackNavigationPolicy.PlayerBackAction.TRY_AUDIO_SOURCE;
+import static me.aap.fermata.ui.view.BodyLayout.Mode.BOTH;
+import static me.aap.fermata.ui.view.BodyLayout.Mode.FRAME;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 public class BackNavigationPolicyTest {
-	@Test
-	public void splitBackUsesChildHistoryThenDashboard() {
-		assertEquals(REFRESH_CHROME,
-				BackNavigationPolicy.resolvePlayerBack(true, true, true, false));
-		assertEquals(BackNavigationPolicy.PlayerBackAction.SHOW_DASHBOARD,
-				BackNavigationPolicy.resolvePlayerBack(true, true, false, false));
-		assertEquals(BackNavigationPolicy.PlayerBackAction.SHOW_DASHBOARD,
-				BackNavigationPolicy.resolvePlayerBack(true, false, false, false));
-	}
-
-	@Test
-	public void fullscreenBackLeavesVideoBeforeAudioOrActivityFallback() {
-		assertEquals(BackNavigationPolicy.PlayerBackAction.HANDLED,
-				BackNavigationPolicy.resolvePlayerBack(false, true, true, true));
-		assertEquals(LEAVE_VIDEO_MODE,
-				BackNavigationPolicy.resolvePlayerBack(false, false, false, true));
-		assertEquals(TRY_AUDIO_SOURCE,
-				BackNavigationPolicy.resolvePlayerBack(false, false, false, false));
-	}
-
 	@Test
 	public void fullscreenExitHonorsFragmentSplitCapability() {
 		assertEquals(BOTH, BackNavigationPolicy.resolveVideoExitMode(true));
