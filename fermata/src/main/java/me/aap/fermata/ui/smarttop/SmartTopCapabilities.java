b1c1eb90fa423bd3906fd29a01b81204d88ef983
chuoinho <108043501+chuoinho@users.noreply.github.com>
2026-08-21 12:37:10 +0700

    refactor(smarttop): remove previous capability

--- fermata/src/main/java/me/aap/fermata/ui/smarttop/SmartTopCapabilities.java
+++ fermata/src/main/java/me/aap/fermata/ui/smarttop/SmartTopCapabilities.java
@@ -1,16 +1,14 @@
 package me.aap.fermata.ui.smarttop;
 
 /** Addon-neutral capabilities used to derive the remaining SmartTop controls. */
-public record SmartTopCapabilities(
-		boolean canSkipPrevious,
-		boolean canFavorite) {
-	public static final SmartTopCapabilities NONE = new SmartTopCapabilities(false, false);
+public record SmartTopCapabilities(boolean canFavorite) {
+	public static final SmartTopCapabilities NONE = new SmartTopCapabilities(false);
 
 	public static SmartTopCapabilities current(boolean favorite) {
-		return new SmartTopCapabilities(true, favorite);
+		return new SmartTopCapabilities(favorite);
 	}
 
 	public static SmartTopCapabilities suggestion(boolean favorite) {
-		return new SmartTopCapabilities(false, favorite);
+		return new SmartTopCapabilities(favorite);
 	}
 }
